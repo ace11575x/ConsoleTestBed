aa73367e3b43e4d0f169e2fe917b3e30cfba4e9c
Joseph Hempstead <ace11575x@gmail.com>
2020-05-08 16:23:49 -0500

    1st Commit for ChadP

---
+++ ConsoleTestBed/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace ConsoleTestBed
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      using (WebClient client = new WebClient())
+      {
+        client.DownloadFile("https://downloads.malwarebytes.com/file/adwcleaner", @"C:\Code Testing\ADW.exe");
+      }
+
+      var versioninfo = FileVersionInfo.GetVersionInfo(@"C:\Code Testing\ADW.exe");
+      string version = versioninfo.FileVersion;
+
+      
+      Console.WriteLine(versioninfo.FileDescription  + " Version Number " + versioninfo.FileVersion +  " original File Name " + versioninfo.OriginalFilename);
+
+        Console.ReadKey();
+    }
+  }
+}
